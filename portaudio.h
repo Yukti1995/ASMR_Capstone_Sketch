939a3a115637e2c700ef40b287d269090efe14cb
Yukti1995 <55197733+Yukti1995@users.noreply.github.com>
2020-03-17 22:24:58 -0400

    Create portaudio.h

---
+++ portaudio.h
@@ -0,0 +1,9 @@
+Callback Function 
+
+
+typedef int PaStreamCallback( const void *input,
+                                      void *output,
+                                      unsigned long frameCount,
+                                      const PaStreamCallbackTimeInfo* timeInfo,
+                                      PaStreamCallbackFlags statusFlags,
+                                      void *userData ) ;
